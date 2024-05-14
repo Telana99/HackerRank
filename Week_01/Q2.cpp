eb8cde14b3105fa0fcd390c7e215c778168f0812
Telana99 <139704352+Telana99@users.noreply.github.com>
2024-05-14 21:26:03 +0530

    Q1 and Q2 are done

---
+++ Week_01/Q2.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+   int n1,n2,n3,sum;
+   cin >> n1 >> n2 >> n3;
+
+   sum = n1+n2+n3;
+
+   cout << sum << endl;
+
+
+   
+    return 0;
+}
